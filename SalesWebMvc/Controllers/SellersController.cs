0bafd0a0585d1197b8b0bd2ea7beba98a559b3b7
Joao Campanharo <campanharo@gmail.com>
2018-12-06 05:38:27 -0200

    Delete seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -40,5 +40,29 @@ namespace SalesWebMvc.Controllers
             __sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = __sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            __sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
